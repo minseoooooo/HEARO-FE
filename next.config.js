330afc0231a8a80cbef87a765bb094668a7bf88e
minseoooooo <oesnimnos@gmail.com>
2025-10-13 17:01:41 +0900

    Revert "update: modify Bitbucket pipeline for automatic output generation and adjust Next.js config for static export"
    This reverts commit 6ed05b5f74bf774c07c92679ddaec7ffdaaef675.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // ✅ 정적 사이트로 내보내기
-    images: {
-        unoptimized: true, // ✅ export 모드에선 필요 (Next Image 최적화 끔)
-    },
-};
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
