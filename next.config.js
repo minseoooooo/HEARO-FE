6ed05b5f74bf774c07c92679ddaec7ffdaaef675
minseoooooo <oesnimnos@gmail.com>
2025-10-13 16:16:12 +0900

    update: modify Bitbucket pipeline for automatic output generation and adjust Next.js config for static export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
-}
+    output: 'export', // ✅ 정적 사이트로 내보내기
+    images: {
+        unoptimized: true, // ✅ export 모드에선 필요 (Next Image 최적화 끔)
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
