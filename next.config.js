ce372a27b09597474b71db56260eb8fd57a4f3ec
minseoooooo <oesnimnos@gmail.com>
2025-10-13 17:00:38 +0900

    update: next.config.js to enable static site export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
+    output: 'export', // ✅ 정적 사이트로 내보내기
+    eslint: {ignoreDuringBuilds: true},
+    typescript: {ignoreBuildErrors: true},
+    images: {unoptimized: true},
 }
 
 module.exports = nextConfig
