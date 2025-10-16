05884d9174d2d2961d0a965298ed823704bc5b65
minseoooooo <oesnimnos@gmail.com>
2025-10-16 21:34:21 +0900

    chore: enable trailing slash in next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
     eslint: {ignoreDuringBuilds: true},
     typescript: {ignoreBuildErrors: true},
     images: {unoptimized: true},
+    trailingSlash: true,
 }
 
 module.exports = nextConfig
